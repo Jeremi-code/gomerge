a87c76a0b0bf4e82f16082f4cd7d8127b88da3df
Ermias Sintayehu <ermiassintayehu13@gmail.com>
2025-04-30 00:09:21 +0300

    refactor: make unit command running function

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
-	"flag"
+	// "flag"
+	"fmt"
 	"github.com/jeremi-code/gomerge/app"
+	"os"
 )
 
 func main() {
-	branch := flag.String("branch", "", "Branch name to start the app with")
-	flag.Parse()
 
-	app.Start(*branch)
+	if len(os.Args) == 1 {
+		branchName,err:=app.GetBranch()
+		if(err!=nil){
+			panic(err)
+		}
+		fmt.Println("Current branch name:", branchName)
+		
+	}
+	fmt.Println("Starting the app...", os.Args[0])
+	// branch := flag.String("branch", "", "Branch name to start the app with")
+	// flag.Parse()
+
+	// app.Start(*branch)
 
 }
