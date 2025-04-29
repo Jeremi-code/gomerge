392716ccbc5f203ab2cf433563cc9c2740f844df
Ermias Sintayehu <ermiassintayehu13@gmail.com>
2025-04-29 22:06:43 +0300

    feat: functions for merging, pulling and getting branch

--- app/app.go
+++ app/app.go
@@ -1,12 +1,17 @@
 package app
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
 )
+
+type Config struct {
+	BranchName string `json:"branch"`
+}
 
 func Start(branch_name string) {
 	var isBackend bool = isBackend()
@@ -44,4 +49,38 @@ func switchBranch(branchName string) {
 			fmt.Println("Error switching branches:", err)
 		}
 	}
+}
+
+func pullBranch() {
+	err := exec.Command("git", "pull").Run()
+	if err != nil {
+		fmt.Println("Error pulling branch:", err)
+	}
+}
+
+func mergeBranch(branchName string) {
+	err := exec.Command("git", "switch", branchName).Run()
+	if err != nil {
+		fmt.Println("Error switching to branch:", err)
+		return
+	}
+	err = exec.Command("git", "merge", branchName).Run()
+	if err != nil {
+		fmt.Println("Error merging branch:", err)
+	}
+}
+
+func GetBranch() (string, error) {
+	dat, err := os.ReadFile(".myapprc")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return "", err
+	}
+	var config Config
+	err = json.Unmarshal(dat, &config)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+		return "", err
+	}
+	return config.BranchName, nil
 }
