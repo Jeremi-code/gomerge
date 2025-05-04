5991161aedb6bb5ef1d57ed13d65e7169c46b886
Ermias Sintayehu <ermiassintayehu13@gmail.com>
2025-05-04 15:42:23 +0300

    fix: open ghostty with appropriate flag

--- app/terminal.go
+++ app/terminal.go
@@ -9,13 +9,19 @@ import (
 func RunCommand(command string) {
 	cmd := exec.Command("bash", "-c", command)
 	output, err := cmd.CombinedOutput()
+	fmt.Println("Running command:", command)
 	if err != nil {
+		fmt.Println("Error executing command:", command)
 		log.Fatal(err)
 	}
 	fmt.Println(string(output))
 }
 
 func OpenTerminal(command string) {
-	RunCommand("ghostty -c " + command)
+	cmd := exec.Command("ghostty", "-e", command)
+    err := cmd.Start()
+    if err != nil {
+        log.Fatalf("Failed to start Ghostty: %v", err)
+    }
 
 }
