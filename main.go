eac945079f7f70c7e57f6f9b36b2de8e7db9a9d8
Ermias Sintayehu <ermiassintayehu13@gmail.com>
2025-05-04 16:01:31 +0300

    feat: handle when argument length is 2

--- main.go
+++ main.go
@@ -1,29 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"os"
-	"strings"
+
 	"github.com/jeremi-code/gomerge/app"
 )
 
 func main() {
-
+	fmt.Println("Welcome to GoMerge")
 	if len(os.Args) == 2 {
-		dir := app.GetCurrentDirectory()
-		workFolder := app.ReadConfigFile().FolderDirectory
-		if strings.Contains(dir, workFolder) {
-			app.RunCommand("cd" + " " + os.Args[1])
-			isBackend := app.IsBackend()
-			if isBackend {
-				app.UploadMigration()
-				app.SwitchBranch("dev")
-				app.MergeBranch("dev", "fix/issues")
-			} else {
-				app.SwitchBranch("div")
-				app.MergeBranch("dev", "fix/issues")
-			}
-
-		}
-
+		app.Start()
 	}
 }
