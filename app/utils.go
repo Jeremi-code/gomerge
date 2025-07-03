f9fd477008c954ad6be0bbb78327082035881403
Ermias Sintayehu <ermiassintayehu13@gmail.com>
2025-07-03 23:10:52 +0300

    refactor: remove irrelevant code

--- app/utils.go
+++ app/utils.go
@@ -1,45 +1,43 @@
 package app
 
 import (
-	"log"
-	"os"
 	"strings"
 )
 
-func IsBackend() bool {
-	fileName := GetFileName()
-	var isBackend = strings.Contains(fileName, "api")
+func IsBackend(folderName string) bool {
+	var isBackend = strings.Contains(folderName, "api")
 	return isBackend
 }
 
-func GetBranch() (string, error) {
-	config := ReadConfigFile()
-	return config.BranchName, nil
-}
+// func GetBranch() (string, error) {
+// 	config := ReadConfigFile()
+// 	return config.BranchName, nil
+// }
 
-func GetCurrentDirectory() string {
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return dir
-}
+// func GetCurrentDirectory() string {
+// 	dir, err := os.Getwd()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	return dir
+// }
 
-func GetFileName() string {
-	dir := GetCurrentDirectory()
-	var dirSplit = strings.SplitAfter(dir, "/")
-	var fileName = dirSplit[len(dirSplit)-1]
-	if len(fileName) == 0 {
-		log.Fatal("Error: File name is empty")
-	}
-	return fileName
-}
+// func GetFileName() string {
+// 	dir := GetCurrentDirectory()
+// 	var dirSplit = strings.SplitAfter(dir, "/")
+// 	var fileName = dirSplit[len(dirSplit)-1]
+// 	if len(fileName) == 0 {
+// 		log.Fatal("Error: File name is empty")
+// 	}
+// 	return fileName
+// }
 
-func RunWorkflow(branch string, isBackend bool) error {
+func RunWorkflow(branch string,folderName string) error {
+	WriteConfigFile(folderName)
     SwitchBranch("dev")
     PullBranch()
     MergeBranch("dev", branch)
-    if isBackend {
+    if IsBackend(folderName) {
         UploadMigration()
     }
     MergeBranch(branch, "dev")
