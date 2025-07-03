a152cd52bbdfcedec59308d8eb8e550bb68f75f0
Ermias Sintayehu <ermiassintayehu13@gmail.com>
2025-07-04 00:38:04 +0300

    fix: shorthand flag representation has a bug

--- main.go
+++ main.go
@@ -8,22 +8,26 @@ import (
 
 func main() {
 	showRecent := flag.Bool("recent", false, "Open recent file")
-	folderName := flag.String("folder", "", "Folder to open")
-	branchName := flag.String("branch", "", "Branch to use")
 	showRecentShort := flag.Bool("r", false, "Open recent file (short)")
+
+	folderName := flag.String("folder", "", "Folder to open")
 	folderNameShort := flag.String("f", "", "Folder to open (short)")
+
+	branchName := flag.String("branch", "", "Branch to use")
 	branchNameShort := flag.String("b", "", "Branch to use (short)")
-	config := app.ReadConfigFile()
+
 	flag.Parse()
 
-	if *branchName != "" {
-		config.BranchName = *branchName
-		app.WriteConfigFile(config)
-		fmt.Println("Branch is written")
-	} else if *branchNameShort != "" {
+	config := app.ReadConfigFile()
+
+	if *branchNameShort != "" {
 		config.BranchName = *branchNameShort
-		app.WriteConfigFile(config)
 		fmt.Println("Branch is written")
+		app.WriteConfigFile(config)
+	} else if *branchName != "" {
+		config.BranchName = *branchName
+		fmt.Println("Branch is written")
+		app.WriteConfigFile(config)
 	}
 
 	var targetFolder string
@@ -35,10 +39,17 @@ func main() {
 			return
 		}
 		targetFolder = config.RecentFolder
+
 	case *folderName != "" || *folderNameShort != "":
-		targetFolder = *folderName
+		if *folderNameShort != "" {
+			targetFolder = *folderNameShort
+		} else {
+			targetFolder = *folderName
+		}
+
 	case flag.NArg() > 0:
 		targetFolder = flag.Arg(0)
+
 	default:
 		fmt.Println("No file or folder specified, starting app...")
 		return
