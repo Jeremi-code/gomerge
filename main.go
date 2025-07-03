56ef959e3127b04c6d0f36b83e84aed5214a3813
Ermias Sintayehu <ermiassintayehu13@gmail.com>
2025-07-03 23:10:31 +0300

    chore: define recent and folder flags

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
-
 	"github.com/jeremi-code/gomerge/app"
 )
 
 func main() {
-	fmt.Println("Welcome to GoMerge")
-	if len(os.Args) == 2 {
-		app.Start()
+	showRecent := flag.Bool("recent", false, "Open recent file")
+	folderName := flag.String("folder", "", "Folder to open")
+	config := app.ReadConfigFile()
+	flag.Parse()
+
+	var targetFolder string
+
+	switch {
+	case *showRecent:
+		if config.RecentFolder == "" {
+			fmt.Println("No recent folder found.", config)
+			return
+		}
+		fmt.Println("what is then")
+		targetFolder = config.RecentFolder
+	case *folderName != "":
+		targetFolder = *folderName
+	case flag.NArg() > 0:
+		targetFolder = flag.Arg(0)
+	default:
+		fmt.Println("No file or folder specified, starting app...")
+		return
 	}
+
+	app.Start(&config, targetFolder)
 }
